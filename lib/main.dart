23876b1b1cd60dd16cc92b1df60bcfec5a5c4df3
samybej <samy.bejaoui@esprit.tn>
2023-06-23 16:08:26 +0200

    popups and signout functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:takemynotes/firebase_options.dart';
 import 'package:takemynotes/views/login_view.dart';
 import 'package:takemynotes/views/register_view.dart';
+import 'package:takemynotes/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log; //only log function is imported
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,19 +37,98 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            /*  final user = FirebaseAuth.instance.currentUser;
-              print(user);
-              final emailVerified = user?.emailVerified ?? false;
-              if (emailVerified) {
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NoteTakingView();
               } else {
                 return const VerifyEmailView();
               }
-              return const Text('Done');*/
-            return const LoginView();
+            } else {
+              return const LoginView();
+            }
           default:
             return const CircularProgressIndicator(); //if internet connection is slow this is the first thing that appears before the user/email column
         }
       },
     );
   }
+}
+
+enum MenuAction {
+  logout
+} //will be used for the PopupMenuButton & PopupMenuItem
+
+class NoteTakingView extends StatefulWidget {
+  const NoteTakingView({super.key});
+
+  @override
+  State<NoteTakingView> createState() => _NoteTakingViewState();
+}
+
+class _NoteTakingViewState extends State<NoteTakingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes Page'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  devtools.log(shouldLogout.toString());
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    if (context.mounted) {
+                      Navigator.of(context)
+                          .pushNamedAndRemoveUntil('/login/', (route) => false);
+                    }
+                  }
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Logout'),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('These are my notes...'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(false); //basically return the value false
+              },
+              child: const Text('Cancel')),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Logout'),
+          ),
+        ],
+      );
+    },
+  ).then((value) =>
+      value ??
+      false); // in case showDialog<bool> returns a null value we return false! we also could change the type of the method to Future<bool?>
 }
