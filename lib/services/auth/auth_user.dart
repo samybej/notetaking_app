20e5de2a7520ea420b898b3fc75607fbd3b4eb0f
samybej <samy.bejaoui@esprit.tn>
2023-06-23 23:04:46 +0200

    testing basic unit tests

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,7 +5,9 @@ import 'package:flutter/foundation.dart';
 class AuthUser {
   final bool isEmailVerified;
 
-  const AuthUser(this.isEmailVerified);
+  const AuthUser(
+      {required this.isEmailVerified}); //required means we have to specify the attribute name when constructing an instance.
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
